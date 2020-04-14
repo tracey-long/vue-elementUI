d34d402ef21bbae15fbec5f55d4ef2a0266a61e6
dengmiaomiao <tracey_long@163.com>
2020-04-14 15:18:24 +0800

    发布el-element-image并全部引入成功

--- vue-elementui/src/main.js
+++ vue-elementui/src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import { ElImage } from "element-ui-image";
+import Element from "element-ui-image";
+import "element-ui-image/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false;
+Vue.use(Element);
+// Vue.use(ElImage);
 
 new Vue({
   router,
--- vue-elementui/src/views/Home.vue
+++ vue-elementui/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="demo-image__preview">
+      <el-image
+        style="width: 100px; height: 100px"
+        :src="url"
+        :preview-src-list="srcList"
+      >
+      </el-image>
+    </div>
+    <!-- <hello-world></hello-world> -->
+    <el-button type="text">el-button</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      url:
+        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      srcList: [
+        "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
+        "https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg"
+      ]
+    };
   }
 };
 </script>
